96c2c050e5efc0a3ac4eead004a90535039d8782
Lesia Kiseleva <lesia.kiseleva@gmail.com>
2020-12-03 15:33:24 +0300

    start

---
+++ ft_printf.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbrenton <sbrenton@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 14:38:40 by sbrenton          #+#    #+#             */
+/*   Updated: 2020/12/03 15:30:43 by lesia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+
+//[flags][width][.precision][length]specifier
+
+int ft_printf(void)
+{
+	printf("start");
+	return (0);
+}
+
+
+int main(void)
+{
+	printf("start");
+	return (0);
+}
---
+++ ft_printf.h
@@ -0,0 +1,8 @@
+//
+// Created by Lesia Kiseleva on 03.12.2020.
+//
+
+#ifndef FT_PRINTF_FT_PRINTF_H
+#define FT_PRINTF_FT_PRINTF_H
+
+#endif //FT_PRINTF_FT_PRINTF_H
