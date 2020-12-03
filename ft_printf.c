3ed78cc4826f58e0c618255ffed88454695ba915
Lesia Kiseleva <lesia.kiseleva@gmail.com>
2020-12-03 15:42:56 +0300

    start

--- ft_printf.c
+++ ft_printf.c
@@ -6,21 +6,20 @@
 /*   By: sbrenton <sbrenton@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 14:38:40 by sbrenton          #+#    #+#             */
-/*   Updated: 2020/12/03 15:30:43 by lesia            ###   ########.fr       */
+/*   Updated: 2020/12/03 15:39:55 by lesia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-#include <stdio.h>
+# include "ft_printf.h"
+# include <stdio.h>
 
 //[flags][width][.precision][length]specifier
 
-int ft_printf(void)
-{
-	printf("start");
-	return (0);
-}
-
+//int ft_printf(void)
+//{
+//	printf("start");
+//	return (0);
+//}
 
 int main(void)
 {
--- ft_printf.h
+++ ft_printf.h
@@ -5,4 +5,25 @@
 #ifndef FT_PRINTF_FT_PRINTF_H
 #define FT_PRINTF_FT_PRINTF_H
 
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+
+//typedef struct	s_ppack
+//{
+//	int		minus;
+//	int		zero;
+//	int		prec;
+//	int		width;
+//	int		wasdot;
+//	int		negprec;
+//	int		prectow;
+//	int		error;
+//	char	type;
+//}				t_ppack;
+//
+
+
 #endif //FT_PRINTF_FT_PRINTF_H
